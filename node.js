a371dc9341d0f648953c50b8eee9c2058582f2b8
atmaneouerdia <126773349+atmaneouerdia@users.noreply.github.com>
2023-03-23 11:44:51 +0100

    Add files via upload

--- node.js
+++ node.js
@@ -1,13 +1,15 @@
 
 
-console.log("NodeJs");
-function additionner(a, b) {
-    
-    if (a === 2){
-        return a + b;
+function calculerMoyenne(notes) {
+    let total = 0;
+    for (let i = 0; i < notes.length; i++) {
+      total = total + notes[i];
+    }
+    let moyenne = total / notes.length;
+    return moyenne;
   }
   
-  let resultat = additionner(3, 5);
+  let notes = [12, 15, 10, 18, 16];
+  let moyenne = calculerMoyenne(notes);
   
-  console.log(resultat);
-}
+  console.log("La moyenne est de " + moyenne);
