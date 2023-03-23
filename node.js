dc210ab1ee539059d60cff4022e73778cd330c8d
atmaneouerdia <126773349+atmaneouerdia@users.noreply.github.com>
2023-03-23 10:35:27 +0100

    Add files via upload

---
+++ node.js
@@ -0,0 +1,13 @@
+
+
+console.log("NodeJs");
+function additionner(a, b) {
+    
+    if (a === 2){
+        return a + b;
+  }
+  
+  let resultat = additionner(3, 5);
+  
+  console.log(resultat);
+}
